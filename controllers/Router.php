7d771a8885f8bb4dc257b0011403f920d8d8f092
Cordial <alicia.cordial@laplateforme.io>
2021-05-18 15:30:25 +0200

    début barre de recherche
    marche jusqu'à chercher dans bdd

--- controllers/Router.php
+++ controllers/Router.php
@@ -13,6 +13,7 @@ class Router{
             //chargement automatique des classes
             spl_autoload_register(function($class){
                 require_once('models/'.$class.'.php');
+                
             });
 
             $url = '';
